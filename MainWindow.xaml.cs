efc9a0c328e2accf6fc2cead2253438738e9a31c
Isaak <43730681+I5UCC@users.noreply.github.com>
2022-08-15 19:00:11 +0200

    Continuous typing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,7 +14,6 @@ namespace VRCTextboxOSC
     public partial class MainWindow : Window
     {
         private Timer intervalTimer;
-        private double rateLimit;
         private UDPSender oscSender;
         private bool isInitialized = false;
         private IniData iniData;
@@ -33,135 +32,130 @@ namespace VRCTextboxOSC
             intervalTimer = new(double.Parse(iniData["Settings"]["Rate"]));
             intervalTimer.Elapsed += Time_Elapsed;
 
-            TbxRate.Text = iniData["Settings"]["Rate"];
             CbxModes.SelectedIndex = int.Parse(iniData["Settings"]["Mode"]);
             
-            rateLimit = int.Parse(iniData["VRCLimits"]["RateLimit"]);
-            TbxMain.MaxLength = int.Parse(iniData["VRCLimits"]["MaxLength"]);
+            CkbxOverflow.IsChecked = bool.Parse(iniData["Settings"]["Continuous"]);
 
-            Button_Send.Visibility = Visibility.Hidden;
+            SettingsChanged();
         }
 
-        private void Time_Elapsed(object? s, ElapsedEventArgs e) => SendMessage();
+        private void Window_Closed(object s, EventArgs e) => iniParser.WriteFile(CONFIGPATH, iniData);
 
         private void Grid_MouseDown(object s, MouseButtonEventArgs e) => grid.Focus();
 
-        private void Button_Clear_Click(object s, RoutedEventArgs e) => ClearMessage();
+        private void Time_Elapsed(object? s, ElapsedEventArgs e) => SendMessage(false);
 
-        private void Button_Send_Click(object s, RoutedEventArgs e) => SendMessage();
+        private void Button_Send_Click(object s, RoutedEventArgs e) => SendMessage(true);
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) => SettingsChanged();
 
         private void TbxRate_LostFocus(object s, RoutedEventArgs e) => SettingsChanged();
 
         private void CbxModes_SelectionChanged(object s, SelectionChangedEventArgs e) => SettingsChanged();
 
-        private void TextBox_TextChanged(object s, TextChangedEventArgs e)
-        {
-            if (isInitialized)
-            {
-                if (TbxMain.Text.Length == 0)
-                {
-                    ClearMessage();
-                }
-                else
-                {
-                    oscSender.Send(new OscMessage("/chatbox/typing", true));
-
-                    if (CbxModes.SelectedIndex == 0)
-                        intervalTimer.Start();
-                }
-
-                LblCharCount.Content = String.Format("{0}/{1}", TbxMain.Text.Length, TbxMain.MaxLength);
-                if (TbxMain.Text.Length >= 120)
-                    LblCharCount.Foreground = new SolidColorBrush(Colors.Red);
-                else if (TbxMain.Text.Length >= 90)
-                    LblCharCount.Foreground = new SolidColorBrush(Colors.Orange);
-                else
-                    LblCharCount.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#666666"));
-            }
-        }
-
-        private void SettingsChanged()
-        {
-            if (isInitialized)
-            {
-                if (TbxRate.Text == "" || TbxRate.Text == "0")
-                    TbxRate.Text = "1";
-
-                iniData["Settings"]["Rate"] = TbxRate.Text;
-                iniData["Settings"]["Mode"] = CbxModes.SelectedIndex.ToString();
-
-                intervalTimer.Interval = Convert.ToDouble(TbxRate.Text);
-
-                switch (CbxModes.SelectedIndex)
-                {
-                    case 0:
-                        LblMs.Visibility = Visibility.Visible;
-                        LblRate.Visibility = Visibility.Visible;
-                        TbxRate.Visibility = Visibility.Visible;
-                        Button_Send.Visibility = Visibility.Hidden;
-                        break;
-                    case 1:
-                        LblMs.Visibility = Visibility.Hidden;
-                        LblRate.Visibility = Visibility.Hidden;
-                        TbxRate.Visibility = Visibility.Hidden;
-                        Button_Send.Visibility = Visibility.Visible;
-                        break;
-                    default:
-                        MessageBox.Show("HUH HOW DID YOU DO THAT?!?!?");
-                        break;
-                }
-            }
-        }
-
-        private void TbxRate_TextChanged(object s, TextChangedEventArgs e)
-        {
-            if (isInitialized)
-            {
-                
-                Regex reg = new("[^0-9]");
-                if (reg.IsMatch(TbxRate.Text) && TbxRate.Text.Length != 0)
-                {
-                    TbxRate.Text = TbxRate.Text.Remove(TbxRate.Text.Length - 1);
-                    TbxRate.Select(TbxRate.Text.Length, 0);
-                }
-
-                if (TbxRate.Text != "" && Convert.ToInt32(TbxRate.Text) < rateLimit)
-                {
-                    LblMs.Content = String.Format("ms < {0} rate limit.", rateLimit);
-                    LblMs.Foreground = new SolidColorBrush(Colors.Red);
-                }
-                else
-                {
-                    LblMs.Content = "ms";
-                    LblMs.Foreground = new SolidColorBrush(Colors.White);
-                }
-            }
-        }
+        private void Button_Clear_Click(object s, RoutedEventArgs e) => ClearMessage();
 
         private void Textbox_KeyDown(object s, KeyEventArgs e)
         {
             if (e.Key == Key.Enter && CbxModes.SelectedIndex == 1)
-                SendMessage();
+                SendMessage(true);
         }
 
-        private void SendMessage() 
+        private void SendMessage(bool clear)
         {
             Dispatcher.Invoke(() =>
             {
                 oscSender.Send(new OscMessage("/chatbox/typing", false));
                 oscSender.Send(new OscMessage("/chatbox/input", TbxMain.Text, true));
                 intervalTimer.Stop();
+                if (clear)
+                    TbxMain.Text = String.Empty;
             });
+
+
         }
 
         private void ClearMessage()
         {
-            TbxMain.Text = "";
-            oscSender.Send(new OscMessage("/chatbox/input", "", true));
+            TbxMain.Text = String.Empty;
+            oscSender.Send(new OscMessage("/chatbox/input", String.Empty, true));
             oscSender.Send(new OscMessage("/chatbox/typing", false));
             intervalTimer.Stop();
         }
 
-        private void Window_Closed(object s, EventArgs e) => iniParser.WriteFile(CONFIGPATH, iniData);
+        private void TbxMain_TextChanged(object s, TextChangedEventArgs e)
+        {
+            if (!isInitialized)
+                return;
+
+            if (TbxMain.MaxLength == TbxMain.Text.Length && CkbxOverflow.IsChecked != null && (bool)CkbxOverflow.IsChecked)
+            {
+                int delIndex = TbxMain.Text.IndexOf(' ') + 1;
+
+                if (delIndex == 0)
+                    TbxMain.Text = TbxMain.Text.Remove(TbxMain.Text.Length - 1);
+                else
+                    TbxMain.Text = TbxMain.Text[delIndex..];
+
+                TbxMain.Select(TbxMain.Text.Length, 0);
+            }
+
+            if (TbxMain.Text.Length == 0)
+            {
+                ClearMessage();
+            }
+            else
+            {
+                oscSender.Send(new OscMessage("/chatbox/typing", true));
+
+                if (CbxModes.SelectedIndex == 0)
+                    intervalTimer.Start();
+            }
+
+            LblCharCount.Content = String.Format("{0}/{1}", TbxMain.Text.Length, TbxMain.MaxLength);
+            if (TbxMain.Text.Length >= 120)
+                LblCharCount.Foreground = new SolidColorBrush(Colors.Red);
+            else if (TbxMain.Text.Length >= 90)
+                LblCharCount.Foreground = new SolidColorBrush(Colors.Orange);
+            else
+                LblCharCount.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#666666"));
+        }
+
+        private void SettingsChanged()
+        {
+            if (!isInitialized)
+                return;
+
+            if (CkbxOverflow.IsChecked != null && (bool)CkbxOverflow.IsChecked)
+            {
+                iniData["Settings"]["Continuous"] = "true";
+                TbxMain.MaxLength = int.Parse(iniData["VRCLimits"]["MaxLength"]) + 1;
+                LblCharCount.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                iniData["Settings"]["Continuous"] = "false";
+                LblCharCount.Visibility = Visibility.Visible;
+                TbxMain.MaxLength = int.Parse(iniData["VRCLimits"]["MaxLength"]);
+            }
+
+            iniData["Settings"]["Mode"] = CbxModes.SelectedIndex.ToString();
+
+            switch (CbxModes.SelectedIndex)
+            {
+                case 0:
+                    CkbxOverflow.Visibility = Visibility.Visible;
+                    Button_Send.Visibility = Visibility.Hidden;
+                    break;
+                case 1:
+                    LblCharCount.Visibility = Visibility.Hidden;
+                    CkbxOverflow.Visibility = Visibility.Hidden;
+                    Button_Send.Visibility = Visibility.Visible;
+                    break;
+                default:
+                    MessageBox.Show("HUH HOW DID YOU DO THAT?!?!?");
+                    break;
+            }
+        }
     }
 }
