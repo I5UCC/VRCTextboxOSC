a3af8b4001528d75c704cfb8f6712f8ad7895436
Isaak <43730681+I5UCC@users.noreply.github.com>
2022-09-25 07:55:22 +0200

    Update AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -9,5 +9,3 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-[assembly: AssemblyVersion("0.1.3")]
-[assembly: AssemblyFileVersion("0.1.3")]
